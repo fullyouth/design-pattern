09595a4420f2d74a8f76f433cfcf34fad3cc8868
zhanghaoqi <zhanghaoqi@shopex.cn>
2021-01-31 19:31:45 +0800

    命令模式

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry: './src/command-pattern/index.tsx',
+  entry: './src/proxy-pattern/index.js',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
